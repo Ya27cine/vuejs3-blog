e32b83d8d5e83fd296169ef218ee958358e15cf7
KHELIFA Yassine <prostam27@gmail.com>
2021-10-03 23:54:28 +0200

    SetUp ::  data & Methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,30 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Home Page</h1>
+
+    <h4>my name is {{ name }} - my age is {{ age }}</h4>
+    <button @click="sayHello">Hello</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
+    name: 'Home',
+
+  setup() {
+    // My data:
+    let name = "Khelifa"
+    let age = 28
+    // Methods: 
+    const sayHello = () => console.log("Welcome in prostam")
+
+    return{
+      name,
+      age,
+      sayHello
+    }
+  },
+ 
  
 }
 </script>
