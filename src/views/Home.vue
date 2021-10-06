9931814d4ac4b36fb7c97ccb0235b75dfd73cad7
KHELIFA Yassine <prostam27@gmail.com>
2021-10-06 21:57:24 +0200

    Using Ref or Reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <h1  ref="title">Home Page</h1>
 
+    <p>{{ socite.name }}</p>
+
     <h4>my name is {{ name }} - my age is {{ age }}</h4>
     <button @click="sayHello">Hello</button>
         <button @click="age++">inc</button>
@@ -11,7 +13,7 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 export default {
     name: 'Home',
 
@@ -20,19 +22,26 @@ export default {
     let name = ref("Khelifa")
     let age = ref(28)
     let title = ref(null);
+
+    let socite = reactive({
+        name: "PROSTAM",
+        siret: "94875856"
+    });
     // Methods: 
     const sayHello = () => {
       console.log("Welcome in prostam")
       title.value.textContent = "Salem"
       title.value.classList.add("text-end")
-      name.value = "Sabah";
+      name.value = "Sabah"
+      socite.name = "SASU PROSTAM"
     }
 
     return{
       name,
       age,
       title,
-      sayHello
+      sayHello,
+      socite
     }
   },
  
