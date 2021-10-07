04ecfa6fd20e1e04f26263400b788852617cb061
KHELIFA Yassine <prostam27@gmail.com>
2021-10-07 22:01:03 +0200

    Life Cycle Hooks

--- src/components/OnePost.vue
+++ src/components/OnePost.vue
@@ -1,19 +1,32 @@
 <template >
 <hr>
-    <h2> {{ post.title }}</h2>
-    <p> {{ extrait }} </p>
+    <h2 @click="show = !show"> {{ post.title }}</h2>
+    <p v-if="show"> {{ extrait }} </p>
 </template>
 
 <script>
-import {computed} from 'vue'
+import {ref, computed, onMounted, onUpdated, onUnmounted} from 'vue'
 export default {
     props: ['post'],
     setup(props) {
         
+        // Life cycle hooks :
+        onMounted( () => console.log('One Post mounted') );
+
+        onUpdated( () => console.log('One Post update') );
+
+         onUnmounted( () => console.log('One Post unmounted') );
+
+
+       // onMounted( () => console.log() );
+
+        const show  = ref(true);
+
         const extrait = computed( () => props.post.content.substring(0, 17)+ '...');
 
         return {
-            extrait
+            extrait,
+            show
         }
     }
 }
